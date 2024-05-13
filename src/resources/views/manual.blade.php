03d3c0666d0ed9aa07452ca7aa6c1a86670dfa3e
Jakub Hubacek <xhubacek@stuba.sk>
2024-05-14 00:31:05 +0200

    manual: download button

--- src/resources/views/manual.blade.php
+++ src/resources/views/manual.blade.php
@@ -6,16 +6,16 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white p-3 shadow-sm sm:rounded-lg flex flex-col justify-center items-center">
                 @if(!auth()->user())
                     @include("pdf.guestPDF")
-                    <a href="{{ route('/pdf/guest') }}">Download PDF</a>
+                    <a href="{{ url('/pdf/guest') }}" class="px-4 py-2 m-2 w-fit bg-blue-500 rounded-md text-white hover:bg-blue-600">Download PDF</a>
                 @elseif (!auth()->user()->isAdmin())
                     @include("pdf.userPDF")
-                    <a href="{{ route('/pdf/user') }}">Download PDF</a>
+                    <a href="{{ url('/pdf/user') }}" class="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">Download PDF</a>
                 @elseif (auth()->user()->isAdmin())
                     @include("pdf.adminPDF")
-                    <a href="{{ url('/pdf/admin') }}">Download PDF</a>
+                    <a href="{{ url('/pdf/admin') }}" class="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">Download PDF</a>
                 @endif
             </div>
         </div>
